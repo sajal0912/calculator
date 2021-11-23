fb25c7f62f6c22ca4c2e4e2ea3d64823613432ca
sajal0912 <sajal.romi@gmail.com>
2021-11-23 01:38:49 -0800

    Fixing the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
+    // using + operator to type cast variables as integer in order to prevent string concatenation
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
